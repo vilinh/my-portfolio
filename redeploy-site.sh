963fb028e1b7d38205f79f155ed60556e3852525
vilinh <vilinhlvu@gmail.com>
2022-06-21 09:15:43 -0700

    redeploy site with systemd service

---
+++ redeploy-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/Vifolio
+git fetch && git reset origin/main --hard
+source python3-virtualenv/bin/activate
+pip install -r requirements.txt
+
+systemctl daemon-reload
+systemctl restart myportfolio
