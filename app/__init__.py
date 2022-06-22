08f3bcb40f32513745243f73fe11f65a7a261b4c
vilinh <vilinhlvu@gmail.com>
2022-06-21 17:54:55 -0700

    css changes on timeline

--- app/__init__.py
+++ app/__init__.py
@@ -6,60 +6,15 @@ from peewee import *
 import datetime
 from playhouse.shortcuts import model_to_dict
 import requests
-import urllib, hashlib
+import urllib
+import hashlib
 
 load_dotenv()
 app = Flask(__name__)
+
 dataf = open("app/static/data.json",  encoding="utf-8")
 data = json.load(dataf)
 
-mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"),
-                     password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
-print(mydb)
-
-# peewee model
-class TimelinePost(Model):
-    name = CharField()
-    email = CharField()
-    content = TextField()
-    created_at = DateTimeField(default=datetime.datetime.now())
-
-    class Meta:
-        database = mydb
-
-mydb.connect()
-mydb.create_tables([TimelinePost])
-
-#timelinepost api
-@app.route('/api/timeline_post', methods=['POST'])
-def post_time_line_post():
-    name = request.form['name']
-    email = request.form['email']
-    content = request.form['content']
-    timeline_post = TimelinePost.create(name=name, email=email, content=content)
-
-    return redirect("/timeline", code=302)
-
-@app.route('/api/timeline_post', methods=['GET'])
-def get_time_line_post():
-    return {
-        'timeline_posts': [
-            model_to_dict(p)
-            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
-        ]
-    }
-
-
-@app.route('/api/timeline_post/<int:id>', methods=['DELETE'])
-def delete_time_line_post_byid(id):
-    TimelinePost.get_by_id(id).delete_instance()
-    return "Successfully deleted"
-
-@app.route('/api/timeline_post/<string:name>', methods=['DELETE'])
-def delete_time_line_post_byname(name):
-    post = TimelinePost.get(TimelinePost.name == name)
-    post.delete_instance()
-    return "Successfully deleted\n"
 
 @app.route('/')
 def index():
@@ -76,6 +31,66 @@ def hobbies():
     hobbies = data['polaroids']
     return render_template('hobbies.html', url=os.getenv("URL"), polaroids=hobbies)
 
+
+# MySQL Database
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"), user=os.getenv("MYSQL_USER"),
+                     password=os.getenv("MYSQL_PASSWORD"), host=os.getenv("MYSQL_HOST"), port=3306)
+
+print(mydb)
+
+# TimelinePost peewee model
+
+
+class TimelinePost(Model):
+    name = CharField()
+    email = CharField()
+    content = TextField()
+    created_at = DateTimeField(default=datetime.datetime.now())
+
+    class Meta:
+        database = mydb
+
+
+mydb.connect()
+mydb.create_tables([TimelinePost])
+
+# TimelinePost api routes
+
+
+@app.route('/api/timeline_post', methods=['GET'])
+def get_time_line_post():
+    return {
+        'timeline_posts': [
+            model_to_dict(p)
+            for p in TimelinePost.select().order_by(TimelinePost.created_at.desc())
+        ]
+    }
+
+
+@app.route('/api/timeline_post', methods=['POST'])
+def post_time_line_post():
+    name = request.form['name']
+    email = request.form['email']
+    content = request.form['content']
+    timeline_post = TimelinePost.create(
+        name=name, email=email, content=content)
+
+    return redirect("/timeline", code=302)
+
+
+@app.route('/api/timeline_post/<int:id>', methods=['DELETE'])
+def delete_time_line_post_byid(id):
+    TimelinePost.get_by_id(id).delete_instance()
+    return "Successfully deleted"
+
+
+@app.route('/api/timeline_post/<string:name>', methods=['DELETE'])
+def delete_time_line_post_byname(name):
+    post = TimelinePost.get(TimelinePost.name == name)
+    post.delete_instance()
+    return "Successfully deleted\n"
+
+
 @app.route('/timeline')
 def timeline():
     posts = get_time_line_post()
@@ -86,8 +101,9 @@ def timeline():
 
     for i in posts["timeline_posts"]:
         email = i["email"].encode('utf-8')
-        gravatar_url = "https://www.gravatar.com/avatar/" + hashlib.md5(email.lower()).hexdigest() + "?"
-        gravatar_url += urllib.parse.urlencode({'d':default, 's':str(size)})
+        gravatar_url = "https://www.gravatar.com/avatar/" + \
+            hashlib.md5(email.lower()).hexdigest() + "?"
+        gravatar_url += urllib.parse.urlencode({'d': default, 's': str(size)})
         pfps.append(gravatar_url)
 
     return render_template('timeline.html', title="Timeline", url=os.getenv("URL"), num=len(posts["timeline_posts"]), posts=posts["timeline_posts"], pfps=pfps)
