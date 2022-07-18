62f7c2bb0059dff622565a2b7640dfaffe0771d0
vilinh <vilinhlvu@gmail.com>
2022-07-18 09:23:53 -0700

    change host env var

--- app/__init__.py
+++ app/__init__.py
@@ -53,7 +53,7 @@ else:
     mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
                          user=os.getenv("MYSQL_USER"),
                          password=os.getenv("MYSQL_PASSWORD"),
-                         host="localhost",
+                         host=os.getENV("MYSQL_HOST"),
                          port=3306)
 
 print(mydb)
