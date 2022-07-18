66af7f4140f9dad151123dd9b16f31e633cf5ba3
vilinh <vilinhlvu@gmail.com>
2022-07-18 14:35:46 -0700

    change test back

--- app/__init__.py
+++ app/__init__.py
@@ -53,7 +53,7 @@ else:
     mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
                          user=os.getenv("MYSQL_USER"),
                          password=os.getenv("MYSQL_PASSWORD"),
-                         host=os.getenv("localhost"),
+                         host=os.getenv("MYSQL_HOST"),
                          port=3306)
 
 print(mydb)
